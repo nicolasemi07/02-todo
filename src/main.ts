839325cd38eb125a7c5179a4dab211b2a3144701
Nicolas <nicolasemi_@hotmail.com>
2024-01-06 19:13:04 -0300

    Seccion 4: Configuro paquetes class-validator y class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Config del paquete class-validator
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
